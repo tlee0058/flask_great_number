7fef5932b2c99d702a6aec77c893be2dfe21f907
tlee0058 <tlee0058@gmail.com>
2018-02-19 00:15:12 -0500

    it works

--- server.py
+++ server.py
@@ -5,21 +5,22 @@ app.secret_key="secret"
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    import random
+    session['randNum'] = random.randrange(0, 101)
+    return render_template("index.html", number=session['number'])
 
 
 @app.route('/guess', methods=['POST'])
 def guess():
-    import random
-    session['randNum'] = random.randrange(0, 101)
 
     guess = int(request.form['number'])
     rNum = session['randNum']
-
-    return redirect ('/result')            
     
-@app.route('/result')
-def result():
-    return render_template("result.html", guess = int(request.form['number']), rNum = session['randNum'])
+    return render_template("result.html", guess = guess, rNum = rNum)
+
+@app.route('/playAgain', methods=['POST'])
+def playAgain():
+    
+    return redirect('/')
 
 app.run(debug=True)
